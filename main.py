06f6ed9a8bb001b91ff3783dfaf365feadc8023b
ShyamalKacha <104887646+ShyamalKacha@users.noreply.github.com>
2025-08-19 19:06:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,34 +3,45 @@ import asyncio
 import requests
 from playwright.async_api import async_playwright
 
-# Telegram bot credentials
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+SCRAPERAPI_KEY = os.getenv("SCRAPERAPI_KEY")
 
-# Function to send Telegram notification
+
 def send_telegram_message(message: str):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": message}
-    try:
-        requests.post(url, data=payload)
-        # print("Notification sent!")
-    except Exception as e:
-        # print("Failed to send Telegram message:", e)
+    requests.post(url, data={"chat_id": CHAT_ID, "text": message})
 
 async def run():
+    target_url = "https://www.flipkart.com/cmf-nothing-phone-2-pro-black-128-gb/p/itm46a119f176627"
+
+    # Wrap target URL with ScraperAPI
+    scraper_url = f"http://api.scraperapi.com?api_key={SCRAPERAPI_KEY}&country_code=in&url={target_url}"
+
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        page = page = await browser.new_page(viewport={"width": 1280, "height": 800},user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36")
-        await page.goto("https://www.flipkart.com/cmf-nothing-phone-2-pro-black-128-gb/p/itm46a119f176627?pid=MOBHAUHA5GRW9FS2&lid=LSTMOBHAUHA5GRW9FS2TVI8FR&marketplace=FLIPKART&q=cmf%20phone%202&sattr[]=color&sattr[]=storage&st=color")
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page(
+            viewport={"width": 1280, "height": 800},
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        )
 
-        # Extract price
-        element = await page.query_selector('div.Nx9bqj.CxhGGd')
-        price = int((await element.inner_text()).replace("₹", "").replace(",", ""))
-        # print("Price:", price)
+        response=await page.goto(scraper_url, wait_until="networkidle")
+        # print("Region:", response.headers.get("scraper-region"))
 
-        # Condition for alert
+        # Wait until the price element loads
+        element = await page.wait_for_selector('div.Nx9bqj.CxhGGd', timeout=30000)
+        price_text = await element.inner_text()
+        price = int(price_text.replace("₹", "").replace(",", ""))
+        # print("Price:", price)  
+
         if price < 19999:
             send_telegram_message(f"📢 Price Drop Alert!\nCMF Phone 2 Pro is now ₹{price}")
+        
+        # target_url="https://ipinfo.io/json"
+        # scraper_url = f"http://api.scraperapi.com?api_key={SCRAPERAPI_KEY}&country_code=in&url={target_url}"
+
+        # await page.goto(scraper_url, wait_until="networkidle")
+        # print(await page.inner_text("pre"))
 
         await browser.close()
 
